70e94dbad3844d6e39e7458e3ae0aa9980800b6d
Nardos <156455050+Nardibelay@users.noreply.github.com>
2024-01-20 21:25:04 +0300

    fibonacci generating
    This is python code to generate the first 20 fibonacci numbers

---
+++ gfn.py
@@ -0,0 +1,17 @@
+#This code is to genrate fibonacci numbers 
+#defining function
+def fibonacci(n):
+    if n <= 0:
+        return []
+    elif n == 1:
+        return [0]
+    elif n == 2:
+        return [0, 1]
+    else:
+        fib_seq = fibonacci(n - 1)
+        fib_seq.append(fib_seq[-1] + fib_seq[-2])
+        return fib_seq
+# Example i usage
+n = 20
+fibonacci_sequence = fibonacci(n)
+print(fibonacci_sequence)
